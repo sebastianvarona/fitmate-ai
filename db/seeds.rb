1598300349d6e7bbbabc38c15ddeb7eda05efd8b
Sebastián Varona <sebastianvarona@Sebastians-MacBook-Air.local>
2025-04-29 16:56:40 -0500

    added seed and migrated schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "### Starting seeds ###"
+
+unless User.exists?
+  users = []
+  users << { email: 'dev@fitmate.com', password: 'dev123', password_confirmation: 'dev123' }
+  print("Creating #{users.size} users...")
+  User.create!(users)
+  puts ' DONE'
+end
