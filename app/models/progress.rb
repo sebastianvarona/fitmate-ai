78ff8ddb71ec75ba265b0180b6ff2ed142b0ebb5
andresjose098 <andres-jose1998@hotmail.com>
2025-05-20 20:48:47 -0500

    commit

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,3 +1,7 @@
 class Progress < ApplicationRecord
   belongs_to :user
+
+  validates :weight, :chest, :arms, :waist, :hip, :thighs, :calves,
+            presence: { message: "no puede estar vacío" },
+            numericality: { greater_than: 0, message: "debe ser un número mayor que 0" }
 end
