8c36ec9ea31f3f21038077353bb9d8fead8cfaed
andresjose098 <andres-jose1998@hotmail.com>
2025-05-27 22:50:58 -0500

    commit validacion progreso

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,3 +1,14 @@
 class Progress < ApplicationRecord
   belongs_to :user
+
+  validates :weight, :chest, :arms, :waist, :hip, :thighs, :calves,
+            presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0,
+              less_than_or_equal_to: 150,
+              message: "debe ser un número entre 0 y 150"
+            }
+
+  validates :user_id, presence: true
 end
